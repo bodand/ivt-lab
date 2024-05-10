00b430f92e5d34e62dcceda091213d44cc54e7d1
András Bodor <bodand@pm.me>
2024-05-10 13:28:49 +0200

    tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -1,40 +1,195 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
-public class GT4500Test {
+class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primary;
+  private TorpedoStore secondary;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primary = mock(TorpedoStore.class);
+    secondary = mock(TorpedoStore.class);
+
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
-  public void fireTorpedo_Single_Success(){
+  void fireTorpedo_Single_Success(){
     // Arrange
+    when(primary.fire(anyInt())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
   }
 
   @Test
-  public void fireTorpedo_All_Success(){
+  void fireTorpedo_All_Success(){
     // Arrange
+    when(primary.fire(anyInt())).thenReturn(true);
+    when(secondary.fire(anyInt())).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(1)).fire(1);
   }
 
+  @Test
+  void fireTorpedo_Single_FirstFire_PrimaryTS_Is_Used() {
+    when(primary.fire(anyInt())).thenReturn(true);
+
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+
+    assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
+    verify(secondary, never()).fire(1);
+  }
+
+  @Test
+  void fireTorpedo_Single_SecondFire_SecondaryTs_Is_Used() {
+    ship.fireTorpedo(FiringMode.SINGLE);
+    reset(primary);
+
+    when(secondary.fire(anyInt())).thenReturn(true);
+
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+
+    assertEquals(true, result);
+    verify(primary, never()).fire(1);
+    verify(secondary, times(1)).fire(1);
+  }
+
+  @Test
+  void fireTorpedo_Single_FirstFire_ButBothEmtpy_None_Used() {
+    when(primary.isEmpty()).thenReturn(true);
+    when(secondary.isEmpty()).thenReturn(true);
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+
+    assertEquals(false, result);
+    verify(primary, never()).fire(1);
+    verify(secondary, never()).fire(1);
+  }
+
+  @Test
+  void fireTorpedo_Single_SecondFire_ButBothEmtpy_None_Used() {
+    ship.fireTorpedo(FiringMode.SINGLE);
+    reset(primary);
+
+    when(primary.isEmpty()).thenReturn(true);
+    when(secondary.isEmpty()).thenReturn(true);
+
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+
+    assertEquals(false, result);
+    verify(primary, never()).fire(1);
+    verify(secondary, never()).fire(1);
+  }
+
+  @Test
+  void fireTorpedo_Single_FirstFire_PrimaryTsEmpty_So_SecondaryTs_Is_Used() {
+    when(primary.isEmpty()).thenReturn(true);
+    when(secondary.fire(anyInt())).thenReturn(true);
+
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+
+    assertEquals(true, result);
+    verify(primary, never()).fire(1);
+    verify(secondary, times(1)).fire(1);
+  }
+
+  @Test
+  void fireTorpedo_Single_SecondFire_SecondaryTsEmpty_So_PrimaryTs_Is_Used() {
+    ship.fireTorpedo(FiringMode.SINGLE);
+    reset(primary);
+
+    when(secondary.isEmpty()).thenReturn(true);
+    when(primary.fire(anyInt())).thenReturn(true);
+
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+
+    assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
+    verify(secondary, never()).fire(1);
+  }
+
+  @Test
+  void fireTorpedo_Single_FirstFire_PrimaryTs_Fails_Secondary_Not_Used() {
+    when(primary.fire(anyInt())).thenReturn(false);
+
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+
+    assertEquals(false, result);
+    verify(primary, times(1)).fire(1);
+    verify(secondary, never()).fire(1);
+  }
+
+  @Test
+  void fireTorpedo_Single_SecondFire_SecondaryTsFails_Primary_Not_Used() {
+    ship.fireTorpedo(FiringMode.SINGLE);
+    reset(primary);
+
+    when(secondary.fire(anyInt())).thenReturn(false);
+
+
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+
+    assertEquals(false, result);
+    verify(primary, never()).fire(1);
+    verify(secondary, times(1)).fire(1);
+  }
+
+  @Test
+  void fireTorpedo_Null_FiriginMode() {
+    assertThrows(NullPointerException.class, () -> {
+      // Act
+      ship.fireTorpedo(null);
+    });
+    verify(primary, never()).fire(anyInt());
+    verify(secondary, never()).fire(anyInt());
+  }
+
+  @Test
+  void fireTorpedo_All_BothTs_Are_Used() {
+    when(primary.fire(anyInt())).thenReturn(true);
+    when(secondary.fire(anyInt())).thenReturn(true);
+
+
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+
+    assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(1)).fire(1);
+  }
 }
